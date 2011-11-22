d696df4ac1e153f30db2aef6118d265ec5a60f14
Zach Leslie <zach@puppetlabs.com>
2011-11-22 14:27:35 -0800

    replace prompt.zsh with my own

--- zsh/prompt.zsh
+++ zsh/prompt.zsh
@@ -1,83 +1,176 @@
-autoload colors && colors
-# cheers, @ehrenmurdick
-# http://github.com/ehrenmurdick/config/blob/master/zsh/prompt.zsh
+#! /bin/zsh
 
-git_branch() {
-  echo $(/usr/bin/git symbolic-ref HEAD 2>/dev/null | awk -F/ {'print $NF'})
-}
+# load in stolen, borrowed, blue, git prompt magic.
+# get the status of the working tree
+git_prompt_status() {
+  OLDIFS=$IFS
+  IFS="\000"
 
-git_dirty() {
-  st=$(/usr/bin/git status 2>/dev/null | tail -n 1)
-  if [[ $st == "" ]]
+  # evil character
+  local ec="€"
+
+  #INDEX=( ${(f)"$(git status --porcelain 2> /dev/null)"} )
+  #INDEX=( $(git status --porcelain 2> /dev/null) )
+  INDEX="${ec}$( git status --porcelain 2> /dev/null | tr "\n" "${ec}" )"
+  STATUS=""
+
+
+  ZSH_THEME_GIT_PROMPT_PREFIX=""
+  ZSH_THEME_GIT_PROMPT_SUFFIX=""
+  ZSH_THEME_GIT_PROMPT_DIRTY=""
+  ZSH_THEME_GIT_PROMPT_CLEAN=""
+  ZSH_THEME_GIT_PROMPT_ADDED="%{$fg[cyan]%}A"
+  ZSH_THEME_GIT_PROMPT_MODIFIED="%{$fg[cyan]%}M"
+  ZSH_THEME_GIT_PROMPT_DELETED="%{$fg[cyan]%}D"
+  ZSH_THEME_GIT_PROMPT_RENAMED="%{$fg[cyan]%}R"
+  ZSH_THEME_GIT_PROMPT_UNMERGED="%{$fg[cyan]%}U"
+  ZSH_THEME_GIT_PROMPT_UNTRACKED="%{$fg[cyan]%}?"
+
+  [[ "${INDEX[@]}" =~ "${ec}\?\? " ]] && STATUS+="$ZSH_THEME_GIT_PROMPT_UNTRACKED"
+
+  if [[ "${INDEX[@]}" =~ "${ec}A  " ]]
   then
-    echo ""
+    STATUS+="$ZSH_THEME_GIT_PROMPT_ADDED"
   else
-    if [[ $st == "nothing to commit (working directory clean)" ]]
+    if [[ "${INDEX[@]}" =~ "${ec}M  " ]]
     then
-      echo "on %{$fg_bold[green]%}$(git_prompt_info)%{$reset_color%}"
-    else
-      echo "on %{$fg_bold[red]%}$(git_prompt_info)%{$reset_color%}"
+      STATUS+="$ZSH_THEME_GIT_PROMPT_MODIFIED"
     fi
   fi
-}
 
-git_prompt_info () {
- ref=$(/usr/bin/git symbolic-ref HEAD 2>/dev/null) || return
-# echo "(%{\e[0;33m%}${ref#refs/heads/}%{\e[0m%})"
- echo "${ref#refs/heads/}"
-}
-
-unpushed () {
-  /usr/bin/git cherry -v origin/$(git_branch) 2>/dev/null
-}
-
-need_push () {
-  if [[ $(unpushed) == "" ]]
+  if [[ "${INDEX[@]}" =~ "${ec} M " ]]
   then
-    echo " "
+    STATUS+="$ZSH_THEME_GIT_PROMPT_MODIFIED"
   else
-    echo " with %{$fg_bold[magenta]%}unpushed%{$reset_color%} "
-  fi
-}
-
-rvm_prompt(){
-  if $(which rvm &> /dev/null)
-  then
-	  echo "%{$fg_bold[yellow]%}$(rvm tools identifier)%{$reset_color%}"
-	else
-	  echo ""
-  fi
-}
-
-# This keeps the number of todos always available the right hand side of my
-# command line. I filter it to only count those tagged as "+next", so it's more
-# of a motivation to clear out the list.
-todo(){
-  if $(which todo.sh &> /dev/null)
-  then
-    num=$(echo $(todo.sh ls +next | wc -l))
-    let todos=num-2
-    if [ $todos != 0 ]
+    if [[ "${INDEX[@]}" =~ "${ec}AM " ]]
     then
-      echo "$todos"
+      STATUS+="$ZSH_THEME_GIT_PROMPT_MODIFIED"
     else
-      echo ""
+      if [[ "${INDEX[@]}" =~ "${ec} T " ]]
+      then
+        STATUS+="$ZSH_THEME_GIT_PROMPT_MODIFIED"
+      fi
     fi
-  else
-    echo ""
   fi
+
+  [[ "${INDEX[@]}" =~ "${ec}R  " ]] && STATUS+="$ZSH_THEME_GIT_PROMPT_RENAMED"
+  [[ "${INDEX[@]}" =~ "${ec} D " ]] && STATUS+="$ZSH_THEME_GIT_PROMPT_DELETED"
+  [[ "${INDEX[@]}" =~ "${ec}UU " ]] && STATUS+="$ZSH_THEME_GIT_PROMPT_UNMERGED"
+
+  echo $STATUS
 }
 
-directory_name(){
-  echo "%{$fg_bold[cyan]%}%1/%\/%{$reset_color%}"
+
+coloratom() {
+  local off=$1 atom=$2
+  if [[ $atom[1] == [[:upper:]] ]]; then
+    off=$(( $off + 60 ))
+  fi
+  echo $(( $off + $colorcode[${(L)atom}] ))
+}
+colorword() {
+  local fg=$1 bg=$2 att=$3
+  local -a s
+
+  if [ -n "$fg" ]; then
+    s+=$(coloratom 30 $fg)
+  fi
+  if [ -n "$bg" ]; then
+    s+=$(coloratom 40 $bg)
+  fi
+  if [ -n "$att" ]; then
+    s+=$attcode[$att]
+  fi
+
+  echo "%{"$'\e['${(j:;:)s}m"%}"
 }
 
-export PROMPT=$'\n$(rvm_prompt) in $(directory_name) $(git_dirty)$(need_push)\n› '
-set_prompt () {
-  export RPROMPT="%{$fg_bold[cyan]%}$(todo)%{$reset_color%}"
+dir_isgit() {
+  pathe=""
+  for ((i=0;i<32;i++)); do
+
+    gitdir="${pathe}.git" 
+    if [ -d "${gitdir}" ]
+    then
+      return 0
+    fi
+
+    pathe="${pathe}../"
+  done
+
+  return -1
 }
 
-precmd() {
-  title "zsh" "%m" "%55<...<%~"
-  set_prompt
+function lprompt {
+
+  #local bracket_open="${color1}${brackets[1]}%f"
+  #local bracket_close="${color1}${brackets[2]}%f"
+  #local git='$vcs_info_msg_0_'
+  #local cwd="${color1}%B%1~%b%f"
+  [[ -n $SSH_TTY ]] && local at="%F{magenta}@%f" || local at="%F{250}@%f"
+  local user_host="%F{240}%n${at}%F{244}%m%F{cyan}:%f"
+
+  #PROMPT="%f${user_host}${cwd}%f $ "
+  PROMPT=
+  #PROMPT+="%F{240}%m%f "
+  PROMPT+="${user_host}"
+  #PROMPT+="%F{014}%(2~.%~.%/)%f"
+  PROMPT+='$vcs_info_msg_1_'
+  PROMPT+="%F{red}%(?.. exited %?)%f"
+  PROMPT+="
+"
+  PROMPT+="%F{014}%#%f "
+
 }
+
+precmd () {
+
+  vcs_info 'prompt'
+  [[ -t 1 ]] || return
+  case $TERM in
+    *xterm*|screen*|rxvt*|(dt|k|E)term) print -Pn "\e]0;%n@%m in %d\a"
+      ;;
+  esac
+
+}
+
+function rprompt {
+  #local brackets=$1
+  #local color1=$2
+  #local color2=$3
+
+  #local bracket_open="${color1}${brackets[1]}%f"
+  #local bracket_close="${color1}${brackets[2]}%f"
+  #local colon="${color1}:"
+  #local at="${color1}@%f"
+
+  #RVM_PROMPT=" %{$fg[yellow]%}${RUBY_VERSION/ruby-/}%{$reset_color%}"
+
+  #[[ -x ~/.rvm/bin/rvm-prompt ]] && ruby_version=$(~/.rvm/bin/rvm-prompt)
+  [[ -n $ruby_version ]] && local rvm="${ruby_version}"
+
+  local git='$vcs_info_msg_0_'
+
+  #local user_host="${color2}%n${at}${color2}%m"
+
+  #local vcs_cwd='${${vcs_info_msg_1_%%.}/$HOME/~}'
+  #local cwd="${color2}%B%20<..<${vcs_cwd}%<<%b"
+  ##local inner="${user_host}${colon}${cwd}"
+  #local inner="${git}"
+
+  RPROMPT=
+  RPROMPT+="${git}"
+  RPROMPT+=" ${rvm}"
+
+  #RPROMPT+=" %F{cyan}%n%f"
+
+  #RPROMPT="%f${bracket_open}${inner}${bracket_close}%f"
+
+  #RPROMPT="${PUPPET_ENV}$RC${RVM_PROMPT}${GIT_PROMPT}${SELINUX}"
+
+}
+
+lprompt
+rprompt
+
+

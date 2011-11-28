2b11343cb683707b6917209d14fab8e5f43d20ab
Zach Leslie <zach@puppetlabs.com>
2011-11-27 20:41:30 -0800

    update comments for aws env

--- aws/env.zsh
+++ aws/env.zsh
@@ -1,7 +1,7 @@
+# AWS related paths and environment variables
 
-# EC2 related paths and environment variables
 if [ -d $HOME/aws ]; then
-# So AWS and EC2 stuff can find the right java
+  # So AWS and EC2 stuff can find the right java
   export JAVA_HOME='/System/Library/Frameworks/JavaVM.framework/Versions/CurrentJDK/Home'
   export AWS_IAM_HOME="$HOME/aws/IAMCli-1.2.0"
   export AWS_CLOUDFORMATION_HOME="${HOME}/aws/AWSCloudFormation-1.0.9"

40dff366740f483dbeea2f7beb8adcf2b4f2ac52
Zach Leslie <zach@puppetlabs.com>
2011-11-28 09:58:22 -0800

    add puppet-module alias

--- puppet/aliases.zsh
+++ puppet/aliases.zsh
@@ -1,2 +1,4 @@
 alias puppet="~/src/puppet/ext/envpuppet puppet"
 alias facter="~/src/puppet/ext/envpuppet facter"
+alias puppet-module="~/src/puppet/ext/envpuppet ~/src/puppet-module-tool/bin/puppet-module"
+

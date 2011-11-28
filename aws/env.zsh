cd11445a903b4e2877a16f7f0d82eb0e62eed04f
Zach Leslie <zach@puppetlabs.com>
2011-11-27 20:39:54 -0800

    update aws info to support cloudformation

--- aws/env.zsh
+++ aws/env.zsh
@@ -1,11 +1,14 @@
-# So AWS and EC2 stuff can find the right java
-export JAVA_HOME='/System/Library/Frameworks/JavaVM.framework/Home'
 
 # EC2 related paths and environment variables
 if [ -d $HOME/aws ]; then
-  #export EC2_HOME="$HOME/aws"
+# So AWS and EC2 stuff can find the right java
+  export JAVA_HOME='/System/Library/Frameworks/JavaVM.framework/Versions/CurrentJDK/Home'
   export AWS_IAM_HOME="$HOME/aws/IAMCli-1.2.0"
+  export AWS_CLOUDFORMATION_HOME="${HOME}/aws/AWSCloudFormation-1.0.9"
   export AWS_CREDENTIAL_FILE="$HOME/.aws/security"
-  [[ -d $AWS_IAM_HOME/bin ]] && PATH="$PATH:$AWS_IAM_HOME/bin"
+  [[ -d ${AWS_IAM_HOME}/bin ]]              && PATH="$PATH:$AWS_IAM_HOME/bin"
+  [[ -d ${AWS_CLOUTFORMATION_HOME}/bin ]] && PATH="$PATH:$AWS_CLOUDFORMATION_HOME/bin"
 fi
+
+#export RUBYLIB=<%= base_dir %>/lib:$RUBYLIB
 

d8b599febe87fd48314a58dc103841b64b126976
Zach Leslie <zach@puppetlabs.com>
2011-11-30 11:11:06 -0800

    update todo zsh aliases

--- todo.txt/aliases.zsh
+++ todo.txt/aliases.zsh
@@ -1,10 +1,4 @@
 # todo.sh: https://github.com/ginatrapani/todo.txt-cli
-function t() { 
-  if [ $# -eq 0 ]; then
-    todo.sh ls
-  else
-    todo.sh $*
-  fi
-}
-
+export TODOTXT_DEFAULT_ACTION=ls
+alias t='todo.sh'
 alias n="t ls +next"

dba035f672d09032bbb0d8bbe3b5df70df81582b
Zach Leslie <zach@puppetlabs.com>
2011-12-02 14:26:01 -0800

    do some reorg on zstyle, and move the file to a name that is loaded after the modules for things like color to work

--- zsh/zstyle.zsh
+++ zsh/zstyle.zsh
@@ -1,6 +1,10 @@
+#--------------------------------------------------------------
+# Globals
+#--------------------------------------------------------------
+colors
+
 # matches case insensitive for lowercase
 zstyle ':completion:*' matcher-list 'm:{a-z}={A-Z}'
-
 zstyle ':completion:*' add-space true
 zstyle ':completion:*' auto-description 'specify: %d'
 zstyle ':completion:*' completer _expand _complete _ignored _match _correct _approximate _prefix
@@ -11,9 +15,35 @@ zstyle ':completion:*' format 'Completing %d'
 zstyle ':completion:*' glob 1
 zstyle ':completion:*' ignore-parents parent pwd ..
 zstyle ':completion:*' insert-unambiguous false
+zstyle ':completion:*' group-name ''
+zstyle ':completion:*' list-colors ${(s.:.)LS_COLORS}
+zstyle ':completion:*' list-prompt %SAt %l \(%p\): Hit TAB for more, or the character to insert%s
+zstyle ':completion:*' matcher-list '' '+m:{[:lower:]}={[:upper:]} m:{[:lower:][:upper:]}={[:upper:][:lower:]}' '+r:|[._-]=** r:|=**' '+l:|=* r:|=*'
+zstyle ':completion:*' match-original both
+zstyle ':completion:*' max-errors 3 numeric
+zstyle -e ':completion:*:approximate:*' max-errors 'reply=($((($#PREFIX+$#SUFFIX)/3))numeric)'
+zstyle ':completion:*' menu select=1
+zstyle ':completion:*' original true
+zstyle ':completion:*' prompt 'Correct %e errors'
+zstyle ':completion:*' select-prompt %SScrolling active: current selection at %l \(%p\)%s
+zstyle ':completion:*' substitute 1
+zstyle ':completion:*' verbose true
+## Use cache
+# Some functions, like _apt and _dpkg, are very slow. You can use a cache in
+# order to proxy the list of results (like the list of available debian
+# packages)
+zstyle ':completion:*' use-cache on
+zstyle ':completion:*' cache-path ${HOME}/.zsh_cache
+
+
+
 zstyle ':completion:*:*:kill:*:processes' list-colors "=(#b) #([0-9]#)*=$color[cyan]=$color[red]"
 
-zstyle ':completion:*:*:git:*:aliases' list-colors "=(#b)(*)  #-- alias for *=$color[none]=$color[green]"
+#--------------------------------------------------------------
+# Git
+#--------------------------------------------------------------
+
+zstyle ':completion:*:*:git:*:aliases'  list-colors "=(#b)(*)  #-- alias for *=$color[none]=$color[green]"
 zstyle ':completion:*:*:git:*:commands' list-colors "=(#b)(*)  #-- *=$color[none]=$color[blue]"
 
 zstyle ':completion:*:*:git-branch:*:branch-names' list-colors "=(#b)((master)#|(origin)(/)(master)#(*)|(*))=$color[blue]=$color[none]=$color[cyan]=$color[green]=$color[none]=$color[cyan]=$color[blue]=$color[blue]"
@@ -42,19 +72,8 @@ zstyle ':completion:*:*:git-add:*:other-files' list-colors "=*=$color[magenta]"
 
 zstyle ':completion:*:default' list-colors 'no=0:fi=0:di=34:ln=36:pi=33:so=35:bd=33:cd=33:or=37;41:su=2;37;41:sg=2;30;43:tw=1;30;42:ow=30;42:st=37:ex=1;32'
 #zstyle ':completion:*:default' list-colors ${(s.:.)LS_COLORS}
-zstyle ':completion:*' group-name ''
-#zstyle ':completion:*' list-colors ''
-zstyle ':completion:*' list-prompt %SAt %l \(%p\): Hit TAB for more, or the character to insert%s
-zstyle ':completion:*' matcher-list '' '+m:{[:lower:]}={[:upper:]} m:{[:lower:][:upper:]}={[:upper:][:lower:]}' '+r:|[._-]=** r:|=**' '+l:|=* r:|=*'
-zstyle ':completion:*' match-original both
-zstyle ':completion:*' max-errors 3 numeric
-zstyle -e ':completion:*:approximate:*' max-errors 'reply=($((($#PREFIX+$#SUFFIX)/3))numeric)'
-zstyle ':completion:*' menu select=1
-zstyle ':completion:*' original true
-zstyle ':completion:*' prompt 'Correct %e errors'
-zstyle ':completion:*' select-prompt %SScrolling active: current selection at %l \(%p\)%s
-zstyle ':completion:*' substitute 1
-zstyle ':completion:*' verbose true
+
+
 zstyle ':completion:*:*:*:*:processes' menu yes select
 zstyle ':completion:*:*:*:*:processes' force-list always
 
@@ -62,12 +81,6 @@ zstyle ':completion:*:*:*:*:processes' force-list always
 # again even if it is already on the command line. To avoid that:
 zstyle ':completion:*:rm:*' ignore-line yes
 
-## Use cache
-# Some functions, like _apt and _dpkg, are very slow. You can use a cache in
-# order to proxy the list of results (like the list of available debian
-# packages)
-zstyle ':completion:*' use-cache on
-zstyle ':completion:*' cache-path ${HOME}/.zsh_cache
 
 zstyle :compinstall filename "${HOME}/.zshrc"
 

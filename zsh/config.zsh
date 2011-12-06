d7d756dfa6b023b3e7d76aee5e9d484ccdd355ee
Zach Leslie <zach@puppetlabs.com>
2011-12-05 20:40:34 -0800

    update LSCOLORS for BSD systems

--- zsh/config.zsh
+++ zsh/config.zsh
@@ -4,7 +4,7 @@ else
   export PS1='%3~$(git_info_for_prompt)%# '
 fi
 
-export LSCOLORS="exfxcxdxbxegedabagacad"
+export LSCOLORS="exgxcxdxcxegedabagacAe"
 export CLICOLOR=true
 
 fpath=($ZSH/zsh/functions $fpath)

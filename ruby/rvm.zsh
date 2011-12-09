a19c99a891a16dc272d9faead41a21fa199bfffe
Zach Leslie <zach@puppetlabs.com>
2011-12-08 16:59:22 -0800

    only echo $ruby_version if its not empty

--- ruby/rvm.zsh
+++ ruby/rvm.zsh
@@ -3,6 +3,6 @@
 # get the name of the branch we are on
 function rvm_prompt_info() {
   ruby_version=$(~/.rvm/bin/rvm-prompt 2> /dev/null) || return
-  echo "($ruby_version)"
+  if [ ! -z $ruby_version ]; then echo "($ruby_version)"; fi
 }
 

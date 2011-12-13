ee61df6503d422b2cf32847a1bdf65c081c9f91d
Zach Leslie <zach@puppetlabs.com>
2011-12-13 09:11:32 -0800

    update colors for less

--- zsh/env.zsh
+++ zsh/env.zsh
@@ -24,13 +24,14 @@ WORDCHARS="${WORDCHARS:s#/#}"
 # Keychain
 eval `keychain -q --eval --agents ssh,gpg --timeout 120 id_rsa`
 
+
 # Less Colors for Man Pages
 export LESS_TERMCAP_mb=$'\E[01;31m'       # begin blinking
-export LESS_TERMCAP_md=$'\E[01;38;5;74m'  # begin bold
+export LESS_TERMCAP_md=$'\E[0;36m'        # begin bold -- cyan
 export LESS_TERMCAP_me=$'\E[0m'           # end mode
 export LESS_TERMCAP_se=$'\E[0m'           # end standout-mode
-export LESS_TERMCAP_so=$'\E[38;5;246m'    # begin standout-mode - info box
+export LESS_TERMCAP_so=$'\E[0;34m'        # begin standout-mode - info box
 export LESS_TERMCAP_ue=$'\E[0m'           # end underline
-export LESS_TERMCAP_us=$'\E[04;38;5;146m' # begin underline
-
+export LESS_TERMCAP_us=$'\E[0;35m'        # begin underline -- purple
+export LESS_TERMCAP_zz=$'\E[0m'           # make 'export' output not now all be colourised.
 

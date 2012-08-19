c8fdb5ffdb3481decec5410edd548ba6874be586
Zach Leslie <zach@puppetlabs.com>
2012-08-19 09:42:05 -0700

    replace mdpwidget with awesompd widget from https://github.com/alexander-yakushev/awesompd

--- X/awesome/rc.lua
+++ X/awesome/rc.lua
@@ -13,6 +13,9 @@ require("debian.menu")
 -- Widgets
 require("vicious")
 
+-- Awesompd
+require("awesompd/awesompd")
+
 
 
 -- {{{ Error handling
@@ -142,10 +145,11 @@ myawesomemenu = {
 
 mymainmenu = awful.menu({ items = { { "awesome", myawesomemenu, beautiful.awesome_icon },
                                     --{ "Debian", debian.menu.Debian_menu.Debian },
+                                    { "terminal", terminal },
                                     { "internet", "chromium" },
                                     { "files", "thunar" },
                                     { "media", mediamenu },
-                                    { "terminal", terminal }
+                                    { "lock", "xscreensaver-command -lock" },
                                   }
                         })
 
@@ -165,6 +169,51 @@ mylauncher = awful.widget.launcher({ image = image(beautiful.awesome_icon),
 -- }}}
 
 -- {{{ Wibox
+
+-- Music Widget
+musicwidget = awesompd:create() -- Create awesompd widget
+musicwidget.font = "Liberation Mono" -- Set widget font 
+musicwidget.scrolling = true -- If true, the text in the widget will be scrolled
+musicwidget.output_size = 30 -- Set the size of widget in symbols
+musicwidget.update_interval = 10 -- Set the update interval in seconds
+-- Set the folder where icons are located (change username to your login name)
+musicwidget.path_to_icons = "/home/zach/.config/awesome/awesompd/icons" 
+-- Set the default music format for Jamendo streams. You can change
+-- this option on the fly in awesompd itself.
+-- possible formats: awesompd.FORMAT_MP3, awesompd.FORMAT_OGG
+musicwidget.jamendo_format = awesompd.FORMAT_MP3
+-- If true, song notifications for Jamendo tracks and local tracks will also contain
+-- album cover image.
+musicwidget.show_album_cover = true
+-- Specify how big in pixels should an album cover be. Maximum value
+-- is 100.
+musicwidget.album_cover_size = 50
+-- This option is necessary if you want the album covers to be shown
+-- for your local tracks.
+musicwidget.mpd_config = "/home/zach/.mpdconf"
+-- Specify the browser you use so awesompd can open links from
+-- Jamendo in it.
+musicwidget.browser = "firefox"
+-- Specify decorators on the left and the right side of the
+-- widget. Or just leave empty strings if you decorate the widget
+-- from outside.
+musicwidget.ldecorator = " "
+musicwidget.rdecorator = " "
+-- Set all the servers to work with (here can be any servers you use)
+musicwidget.servers = { { server = "localhost", port = 6600 } }
+-- Set the buttons of the widget
+musicwidget:register_buttons({ { "", awesompd.MOUSE_LEFT, musicwidget:command_toggle() },
+                    { "Control", awesompd.MOUSE_SCROLL_UP, musicwidget:command_prev_track() },
+              { "Control", awesompd.MOUSE_SCROLL_DOWN, musicwidget:command_next_track() },
+              { "", awesompd.MOUSE_SCROLL_UP, musicwidget:command_volume_up() },
+              { "", awesompd.MOUSE_SCROLL_DOWN, musicwidget:command_volume_down() },
+              { "", awesompd.MOUSE_RIGHT, musicwidget:command_show_menu() },
+                                { "", "XF86AudioLowerVolume", musicwidget:command_volume_down() },
+                                { "", "XF86AudioRaiseVolume", musicwidget:command_volume_up() },
+                                { modkey, "Pause", musicwidget:command_playpause() } })
+musicwidget:run() -- After all configuration is done, run the widget
+
+
  --  Network usage widget
  -- Initialize widget
 netwidget = widget({ type = "textbox" })
@@ -196,16 +245,16 @@ vicious.register(cpuwidget, vicious.widgets.cpu, "$1")
 
 
 -- Initialize widget
-mpdwidget = widget({ type = "textbox" })
--- Register widget
-vicious.register(mpdwidget, vicious.widgets.mpd,
-    function (widget, args)
-        if args["{state}"] == "Stop" then 
-            return " - "
-        else 
-            return args["{Artist}"]..' - '.. args["{Title}"]
-        end
-    end, 10)
+-- mpdwidget = widget({ type = "textbox" })
+-- -- Register widget
+-- vicious.register(mpdwidget, vicious.widgets.mpd,
+--     function (widget, args)
+--         if args["{state}"] == "Stop" then 
+--             return " - "
+--         else 
+--             return args["{Artist}"]..' - '.. args["{Title}"]
+--         end
+--     end, 10)
 
 
 mytextclock = awful.widget.textclock({ align = "right" })
@@ -293,7 +342,8 @@ for s = 1, screen.count() do
         memwidget,
         cpuwidget,
         s == 1 and mysystray or nil,
-        mpdwidget,
+--        mpdwidget,
+        musicwidget.widget,
         mytasklist[s],
         layout = awful.widget.layout.horizontal.rightleft
     }

cc18c80a49ae43be0665f70bd1eee1d116ead5df
Zach Leslie <zach@puppetlabs.com>
2012-12-23 17:51:23 -0800

    scriptify the rbenv installation

--- ruby/rbenv.sh
+++ ruby/rbenv.sh
@@ -1,11 +1,22 @@
 #! /bin/bash
 
+# Zach Leslie <xaque208@gmail.com>
+# 2012-12-23 17:45:46
 
-# Get rbenv up on a new system
-cd
-git clone git://github.com/sstephenson/rbenv.git .rbenv
+# The following script will setup rbenv on a new system.
+
+default_ver="1.9.3-p194"
+rbenv_url="git://github.com/sstephenson/rbenv.git"
+rbenv_build_url="git://github.com/sstephenson/ruby-build.git"
+
+# Clone the rbenv source
+git clone $rbenv_url ~/.rbenv
+
+# Create prepare the build plugin
 mkdir -p ~/.rbenv/plugins
-cd ~/.rbenv/plugins
-git clone git://github.com/sstephenson/ruby-build.git
-rbenv install 1.9.3-p194
+git clone $rbenv_build_url ~/.rbenv/plugins
+
+# install the default version
+rbenv install $default_ver
+rbenv global $default_ver
 

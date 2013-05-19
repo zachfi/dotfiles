e13cad6806164bc7627eed8c62eb516222e2fae9
Zach Leslie <zach@puppetlabs.com>
2013-05-19 12:25:26 -0700

    only use rbenv on systems that have it

--- ruby/rbenv.zsh
+++ ruby/rbenv.zsh
@@ -1,3 +1,10 @@
-PATH="$HOME/.rbenv/bin:$PATH"
-eval "$(rbenv init -)"
-source ~/.rbenv/completions/rbenv.zsh
+
+if [ -d $HOME/.rbenv/bin ]; then
+  PATH="$HOME/.rbenv/bin:$PATH"
+fi
+
+/usr/bin/which -s rbenv
+if [ $? = 0 ]; then
+  eval "$(rbenv init -)"
+  source ~/.rbenv/completions/rbenv.zsh
+fi

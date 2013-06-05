17e1e745d638c1dd830d240d8d95148da61e0b3c
Zach Leslie <zach@puppetlabs.com>
2013-06-05 11:44:00 -0700

    load only what we need to

--- ruby/rbenv.zsh
+++ ruby/rbenv.zsh
@@ -1,10 +1,17 @@
+#
 
-if [ -d $HOME/.rbenv/bin ]; then
-  PATH="$HOME/.rbenv/bin:$PATH"
+# Detect if rbenv has been installed to the homedir
+if [ -d $HOME/.rbenv ]; then
+  # Append to the path
+  if [ -d $HOME/.rbenv/bin ]; then
+    PATH="$HOME/.rbenv/bin:$PATH"
+  fi
+
+  # Load the completions from home
+  /usr/bin/which -s rbenv
+  if [ $? = 0 ]; then
+    eval "$(rbenv init -)"
+    source ~/.rbenv/completions/rbenv.zsh
+  fi
 fi
 
-/usr/bin/which -s rbenv
-if [ $? = 0 ]; then
-  eval "$(rbenv init -)"
-  source ~/.rbenv/completions/rbenv.zsh
-fi

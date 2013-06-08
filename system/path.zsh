11be7deab1d405cd71ad02480560fbfc0d5f3c0f
Zach Leslie <zach@puppetlabs.com>
2013-06-07 23:39:06 -0700

    add shadow/bin to path

--- system/path.zsh
+++ system/path.zsh
@@ -1,4 +1,5 @@
 #export PATH=".:bin:/usr/local/bin:/usr/local/sbin:$HOME/.sfs:$ZSH/bin:$PATH"
 export PATH="$PATH:$HOME/bin"
+export PATH="$PATH:$HOME/shadow/bin"
 
 #export MANPATH="/usr/local/man:/usr/local/mysql/man:/usr/local/git/man:$MANPATH"

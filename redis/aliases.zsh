bffb64b58fc9d5f7cbef43186022cb7de5eb2beb
Zach Leslie <zach@puppetlabs.com>
2013-10-30 21:54:29 -0700

    disable redis aliases

--- redis/aliases.zsh
+++ redis/aliases.zsh
@@ -1,2 +1,4 @@
-alias redis-up='sudo port load redis'
-alias redis-down='sudo port unload redis'
+#alias redis-up='sudo port load redis'
+#alias redis-down='sudo port unload redis'
+#alias redis-up='launchctl load ~/Library/LaunchAgents/homebrew.mxcl.redis.plist'
+#alias redis-down='launchctl unload ~/Library/LaunchAgents/homebrew.mxcl.redis.plist'

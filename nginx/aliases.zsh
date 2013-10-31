7d4f48360629234a727ecc806459e59f6899a23f
Zach Leslie <zach@puppetlabs.com>
2013-10-30 21:53:40 -0700

    stop using nginx aliases

--- nginx/aliases.zsh
+++ nginx/aliases.zsh
@@ -1,2 +1,4 @@
-alias nginx-up="sudo port load nginx"
-alias nginx-down="sudo port unload nginx"
+#alias nginx-up="sudo port load nginx"
+#alias nginx-down="sudo port unload nginx"
+#alias nginx-up="launchctl load ~/Library/LaunchAgents/homebrew.mxcl.nginx.plist"
+#alias nginx-down="launchctl unload ~/Library/LaunchAgents/homebrew.mxcl.nginx.plist"

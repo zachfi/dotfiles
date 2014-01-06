853eef2be6ea76ab9c83879490b6d92069769e07
Zach Leslie <zach@puppetlabs.com>
2014-01-06 15:15:45 -0800

    Begin sourcing repos that are not present

---
+++ bin/sources.py
@@ -0,0 +1,15 @@
+#! /usr/bin/env python
+
+import os
+import subprocess
+
+def setup():
+    sources = {}
+    sources['puppet'] = 'git://github.com/puppetlabs/puppet.git'
+    sources['facter'] = 'git://github.com/puppetlabs/facter.git'
+
+    for k in sources:
+        dest = os.environ['HOME'] + "/src/" + k
+        if not os.path.exists(dest):
+            subprocess.call(['git', 'clone', sources[k], dest])
+

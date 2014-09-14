846aa3669889fc15af43e3bcb461f6f404186c20
Zach Leslie <zach@puppetlabs.com>
2014-09-13 22:37:24 -0700

    Set the default build version

--- ruby/homebrew_install.sh
+++ ruby/homebrew_install.sh
@@ -5,5 +5,5 @@ default_ver="1.9.3-p194"
 brew install rbenv
 brew install ruby-build
 
-#rbenv install $default_ver
-#rbenv global $default_ver
+rbenv install $default_ver
+rbenv global $default_ver

207a828010eb439afcb2bc8e648926ed1104a010
Zach Leslie <zach@puppetlabs.com>
2015-08-23 15:25:24 -0700

    Relocate dotfiles code

---
+++ bin/dotfiles.py
@@ -0,0 +1,37 @@
+import os
+from subprocess import call
+import logging
+
+
+class Dotfiles():
+    def __init__(self, dotfilesRoot):
+        self.dotfilesRoot = dotfilesRoot
+        self.homeDir = os.environ['HOME']
+        self.logger = logging.getLogger('[dotfiles]')
+
+        self.ensureRuby()
+        self.ensureHomebrew()
+        self.ensurePuppet()
+
+    def ensureRuby(self):
+        if not os.path.isdir('{0}/.rbenv'.format(self.homeDir)):
+            self.logger.info('Setting up Ruby...')
+            call(['{}/ruby/install.sh'.format(self.dotfilesRoot)])
+
+    def ensureHomebrew(self):
+        if not os.uname()[0] is 'Darwin':
+            self.logger.info('Skipping homebrew on non-Darwin system')
+            if not os.path.isdir('/usr/local'):
+                call(['{}/osx/homebrew/init.sh'.format(self.dotfilesRoot)])
+
+    def ensurePuppet(self):
+        self.logger.info('begin shadow Puppet')
+        source = '{}/src/puppet/ext/envpuppet'.format(self.homeDir)
+        dest = '{}/bin/envpuppet'.format(self.homeDir)
+        if not os.path.isfile(dest):
+            self.logger.info('copying %s' % source + 'to %s' % dest)
+            copyfile(source,dest)
+            os.chmod(dest, 0o700)
+            call(['{}/.rbenv/shims/gem'.format(self.homeDir), 'install','CFPropertyList'])
+        call(['{}/puppet/go.sh'.format(self.dotfilesRoot)])
+
--- bin/init.py
+++ bin/init.py
@@ -6,38 +6,20 @@ import os
 from subprocess import call
 from shutil import copyfile
 
-def ensureRuby():
-    homedir = os.environ['HOME']
-    if not os.path.isdir('%s/.rbenv' % homedir):
-        call(['%s/dotfiles/ruby/install.sh' % homedir])
-
-def ensureHomebrew():
-    homedir = os.environ['HOME']
-    if not os.path.isdir('/usr/local'):
-        call(['%s/dotfiles/osx/homebrew/init.sh' % homedir])
+from bin.dotfiles import Dotfiles
 
 
 def applyPuppet(manifest):
     homedir = os.environ['HOME']
     call(['%s/bin/envpuppet' % homedir, 'puppet','apply','-v',manifest])
 
-def ensurePuppet():
-    homedir = os.environ['HOME']
-
-    source = '%s/src/puppet/ext/envpuppet' % homedir
-    dest = '%s/bin/envpuppet' % homedir
-    if not os.path.isfile(dest):
-        print('copying %s' % source + 'to %s' % dest)
-        copyfile(source,dest)
-        os.chmod(dest,0700)
-        call(['/Users/zach/.rbenv/shims/gem', 'install','CFPropertyList'])
-    call(['%s/dotfiles/puppet/go.sh' % homedir])
-
 def main():
+    homedir = os.environ['HOME']
+    dotfilesRoot = homedir + '/dotfiles'
+
     symlinks.setup()
     sources.setup()
-    ensureRuby()
-    ensureHomebrew()
-    ensurePuppet()
+
+    d = Dotfiles(dotfilesRoot)
 
 main()

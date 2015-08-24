f9e8f65760ec03c3931abe8b8abe7898540c7bb2
Zach Leslie <zach@puppetlabs.com>
2015-08-24 14:27:32 -0700

    Unfuck pyenv

--- python/env.zsh
+++ python/env.zsh
@@ -1,1 +1,9 @@
-PATH+=":/Users/zach/Library/Python/3.3/bin"
+#PATH+=":/Users/zach/Library/Python/3.3/bin"
+if [ -d $HOME/.pyenv ]; then
+  # Append to the path
+  if [ -d $HOME/.pyenv/bin ]; then
+    PATH="$HOME/.pyenv/bin:$PATH"
+  fi
+fi
+
+if which pyenv > /dev/null; then eval "$(pyenv init -)"; fi

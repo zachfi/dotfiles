e6e520d87c1493431d26961de0048ebe6aa8ad85
Zach Leslie <zach@puppetlabs.com>
2015-11-29 19:20:52 -0800

    Improve zsh alias loading for puppet

--- puppet/aliases.zsh
+++ puppet/aliases.zsh
@@ -1,6 +1,16 @@
-[[ -x ~/src/puppet/ext/envpuppet ]] && alias puppet="~/src/puppet/ext/envpuppet puppet"
-[[ -x ~/src/puppet/ext/envpuppet ]] && alias facter="~/src/puppet/ext/envpuppet facter"
-#[[ -x ~/src/puppet/ext/envpuppet ]] && alias mco="~/src/puppet/ext/envpuppet /Users/zach/src/marionette-collective/mco"
-[[ -x ~/src/puppet/ext/envpuppet ]] &&alias puppet-module="~/src/puppet/ext/envpuppet ~/src/puppet-module-tool/bin/puppet-module"
+if [ -x ~/src/puppet/ext/envpuppet ]; then
+  if [ -d ~/src/puppet ]; then
+    alias puppet="~/src/puppet/ext/envpuppet puppet"
+    alias puppet-module="~/src/puppet/ext/envpuppet ~/src/puppet-module-tool/bin/puppet-module"
+  fi
 
-alias mco="RUBYLIB=~/src/mcollective/lib ~/src/mcollective/bin/mco"
+  if [ -d ~/src/facter ]; then
+    alias facter="~/src/puppet/ext/envpuppet facter"
+  fi
+
+  if [ -d ~/src/mcollective ]; then
+    alias mco="~/src/puppet/ext/envpuppet /Users/zach/src/marionette-collective/mco"
+    alias mco="RUBYLIB=~/src/mcollective/lib ~/src/mcollective/bin/mco"
+  fi
+fi
+

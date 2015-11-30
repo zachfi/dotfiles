81261d1990970dde52323ecbfc091ce993adc64d
Zach Leslie <zach@puppetlabs.com>
2015-11-29 18:48:22 -0800

    Update puppet runner

--- puppet/go.sh
+++ puppet/go.sh
@@ -1,8 +1,20 @@
 #! /bin/bash
 
 MANIFEST=${HOME}/dotfiles/puppet/init.pp
-MODULEPATH=~/dotfiles/puppet/modules:~/dotfiles:~/Code/modules/other
+SITEPP=${HOME}/dotfiles/puppet/site.pp
+MODULEPATH=~/dotfiles/puppet:~/dotfiles/puppet/modules:~/dotfiles:~/Code/modules/other
+ENVPUPPET=~/src/puppet/ext/envpuppet
 
-export FACTER_HOME=${HOME}
+if [ -x $ENVPUPPET ]; then
+	$ENVPUPPET puppet apply --modulepath $MODULEPATH -v $MANIFEST $@
 
-puppet apply --modulepath $MODULEPATH -v $MANIFEST --show_diff $@
+	# R10k
+	. ~/dotfiles/puppet/r10k.zsh
+	r10k puppetfile install -v
+
+	#$ENVPUPPET puppet config set modulepath $MODULEPATH
+	$ENVPUPPET puppet apply --modulepath $MODULEPATH -v $SITEPP $@
+fi
+
+
+

f68acce5b46c90e43f3e0253eaa6ecbd12cd35ec
Zach Leslie <zach@puppetlabs.com>
2015-11-29 19:33:55 -0800

    Detect r10k and shell out if required

--- puppet/go.sh
+++ puppet/go.sh
@@ -1,16 +1,21 @@
 #! /bin/bash
 
-MANIFEST=${HOME}/dotfiles/puppet/first.pp
 SITEPP=${HOME}/dotfiles/puppet/site.pp
 MODULEPATH=~/dotfiles/puppet:~/dotfiles/puppet/modules:~/dotfiles:~/Code/modules/other
 ENVPUPPET=~/src/puppet/ext/envpuppet
 
-if [ -x $ENVPUPPET ]; then
-	$ENVPUPPET puppet apply --modulepath $MODULEPATH -v $MANIFEST $@
+# Ensure r10k is installed
+if [ ! -f ~/.rbenv/shims/r10k ]; then
+	if [ -x ~/.rbenv/shims/gem ]; then
+		/Users/zach/.rbenv/shims/gem install r10k
+	fi
+fi
 
-	# R10k
-	. ~/dotfiles/puppet/r10k.zsh
-	r10k puppetfile install -v
+# Execute the r10k deployment
+. ~/dotfiles/puppet/r10k.zsh
+~/.rbenv/shims/r10k puppetfile install -v
+
+if [ -x $ENVPUPPET ]; then
 
 	#$ENVPUPPET puppet config set modulepath $MODULEPATH
 	$ENVPUPPET puppet apply --modulepath $MODULEPATH -v $SITEPP $@

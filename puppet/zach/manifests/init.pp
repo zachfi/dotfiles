f6066f55fa1bbe259b9be06eeb9962922daad83a
Zach Leslie <zach@puppetlabs.com>
2015-11-29 19:59:37 -0800

    Manage tmux.conf using homedir fact

--- puppet/zach/manifests/init.pp
+++ puppet/zach/manifests/init.pp
@@ -2,4 +2,10 @@ class zach {
   if $::kernel == 'Darwin' {
     include zach::darwin
   }
+
+  file { "${::homedir}/.tmux.conf":
+    owner   => $::id,
+    mode    => '0600',
+    content => template('zach/tmux.conf.erb')
+  }
 }

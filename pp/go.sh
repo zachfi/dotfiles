6276699a456874237117dfce348c9624af168d46
Zach Leslie <zleslie@womply.com>
2015-12-10 08:37:46 -0800

    Fixup on homedir

--- pp/go.sh
+++ pp/go.sh
@@ -6,9 +6,9 @@ ENVPUPPET=~/src/puppet/ext/envpuppet
 
 # Ensure r10k is installed
 if [ ! -f ~/.rbenv/shims/r10k ]; then
-	if [ -x ~/.rbenv/shims/gem ]; then
-		/Users/zach/.rbenv/shims/gem install r10k
-	fi
+    if [ -x ~/.rbenv/shims/gem ]; then
+        ~/.rbenv/shims/gem install r10k
+    fi
 fi
 
 # Execute the r10k deployment
@@ -16,8 +16,8 @@ fi
 ~/.rbenv/shims/r10k puppetfile install -v
 
 if [ -x $ENVPUPPET ]; then
-	#$ENVPUPPET puppet config set modulepath $MODULEPATH
-	$ENVPUPPET puppet apply --modulepath $MODULEPATH -v -e 'include zach' $@
+    #$ENVPUPPET puppet config set modulepath $MODULEPATH
+    $ENVPUPPET puppet apply --modulepath $MODULEPATH -v -e 'include zach' $@
 fi
 
 

1359f92938d5448f74d54df379f8c1db143316fa
Zach Leslie <xaque208@gmail.com>
2016-06-15 16:17:47 -0700

    Clean up prompt code for 5.2

--- zsh/prompt.zsh
+++ zsh/prompt.zsh
@@ -1,37 +1,11 @@
 #! /bin/zsh
 
 
-coloratom() {
-  local off=$1 atom=$2
-  if [[ $atom[1] == [[:upper:]] ]]; then
-    off=$(( $off + 60 ))
-  fi
-  echo $(( $off + $colorcode[${(L)atom}] ))
-}
-
-colorword() {
-  local fg=$1 bg=$2 att=$3
-  local -a s
-
-  if [ -n "$fg" ]; then
-    s+=$(coloratom 30 $fg)
-  fi
-  if [ -n "$bg" ]; then
-    s+=$(coloratom 40 $bg)
-  fi
-  if [ -n "$att" ]; then
-    s+=$attcode[$att]
-  fi
-
-  echo "%{"$'\e['${(j:;:)s}m"%}"
-}
-
 function lprompt {
   [[ -n $SSH_TTY ]] && local at="%F{magenta}@%f" || local at="%F{250}@%f"
   local user_host="%F{240}%n${at}%F{244}%m%F{cyan}:%f"
-  PROMPT=
-  PROMPT+="${user_host}"
-  PROMPT+='$vcs_info_msg_1_'
+  PROMPT=$user_host
+  PROMPT+='${vcs_info_msg_1_}'
   PROMPT+="%F{red}%(?.. exit %?)%f"
   PROMPT+="
 "
@@ -39,28 +13,8 @@ function lprompt {
 
 }
 
-function rprompt {
-  local git='$vcs_info_msg_0_'
-  RPROMPT=
-  RPROMPT+="${git}"
-}
+precmd () { vcs_info 'prompt' }
 
-set_prompt () {
-  rprompt
-}
-
-precmd () {
-
-  vcs_info 'prompt'
-  [[ -t 1 ]] || return
-  case $TERM in
-    *xterm*|screen*|rxvt*|(dt|k|E)term) print -Pn "\e]0;%n@%m in %d\a"
-      ;;
-  esac
-
-  #title "zsh" "%m" "%55<...<%~"
-  set_prompt
-}
-
+RPROMPT='${vcs_info_msg_0_}'
 lprompt
 

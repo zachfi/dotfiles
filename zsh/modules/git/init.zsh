2ec96287685a5ae4cb6c882537458585fb883009
Zach Leslie <zleslie@womply.com>
2017-02-17 11:14:54 -0800

    Set the origin when pushing

--- zsh/modules/git/init.zsh
+++ zsh/modules/git/init.zsh
@@ -3,7 +3,7 @@ if [ -x /usr/local/bin/git ]; then
 fi
 alias gl='git pull --prune'
 alias glog="git log --graph --pretty=format:'%Cred%h%Creset %an: %s - %Creset %C(yellow)%d%Creset %Cgreen(%cr)%Creset' --abbrev-commit --date=relative"
-alias gp='git push origin HEAD'
+alias gp='git push -u origin HEAD'
 alias gd='git diff'
 alias gc='git commit'
 alias gca='git commit -a'

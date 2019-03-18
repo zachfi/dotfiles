9f9f59318724c632b617b63af8d0818735fc6e0f
Zach Leslie <zleslie@newrelic.com>
2019-03-18 09:01:36 -0700

    Add darwin support for fzf

--- zsh/modules/fzf/init.zsh
+++ zsh/modules/fzf/init.zsh
@@ -1,9 +1,19 @@
-if [ -s "/usr/share/fzf/key-bindings.zsh" ]; then
-  source /usr/share/fzf/key-bindings.zsh
+UNAME=$(uname)
+
+if [ $UNAME == "Darwin" ]; then
+  BINDINGS=/usr/local/opt/fzf/shell/key-bindings.zsh
+  COMPLETION=/usr/local/opt/fzf/shell/completion.zsh
+else # Assume Archlinux
+  BINDINGS=/usr/share/fzf/key-bindings.zsh
+  COMPLETION=/usr/share/fzf/key-bindings.zsh
 fi
 
-if [ -s "/usr/share/fzf/key-bindings.zsh" ]; then
-  source /usr/share/fzf/key-bindings.zsh
+if [ -s "${BINDINGS}" ]; then
+  source $BINDINGS
+fi
+
+if [ -s "${COMPLETION}" ]; then
+  source $COMPLETION
 fi
 
 export FZF_DEFAULT_OPTS='--color fg:242,bg:236,hl:65,fg+:15,bg+:239,hl+:108

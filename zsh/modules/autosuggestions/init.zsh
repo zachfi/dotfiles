eb7d305b518ccf24eefa12fb6e47d61e2e0f7948
Zach Leslie <xaque208@gmail.com>
2020-01-15 15:34:32 -0800

    Add FreeBSD support for autosuggestions

--- zsh/modules/autosuggestions/init.zsh
+++ zsh/modules/autosuggestions/init.zsh
@@ -2,6 +2,8 @@ UNAME=$(uname)
 
 if [ "${UNAME}" = "Darwin" ]; then
   SUGGFILE=/usr/local/share/zsh-autosuggestions/zsh-autosuggestions.zsh
+elif [ "${UNAME}" = "FreeBSD" ]; then
+  SUGGFILE=/usr/local/share/zsh-autosuggestions/zsh-autosuggestions.zsh
 else # Assume Archlinux
   SUGGFILE=/usr/share/zsh/plugins/zsh-autosuggestions/zsh-autosuggestions.zsh
 fi

6299e0b1dbef9aed9fe0458e24ee801fdd56dd7c
Zach Leslie <zach.leslie@grafana.com>
2021-10-20 10:38:45 -0600

    Functioanl updates

--- vim/lvim/config.lua
+++ vim/lvim/config.lua
@@ -1,19 +1,9 @@
---[[
-lvim is the global options object
-
-Linters should be
-filled in as strings with either
-a global executable or a path to
-an executable
-]]
--- THESE ARE EXAMPLE CONFIGS FEEL FREE TO CHANGE TO WHATEVER YOU WANT
-
 -- functions
 function find_files_relative(opts)
   opts = opts or {}
   local utils = require "telescope.themes"
   local themes = require "telescope.themes"
-  local theme_opts = themes.get_ivy {
+  local theme_opts = themes.get_dropdown {
     sorting_strategy = "ascending",
     layout_strategy = "bottom_pane",
     prompt_prefix = ">> ",
@@ -31,6 +21,10 @@ lvim.colorscheme = "onedarker"
 
 vim.opt.guifont = "Hack:h12"
 vim.opt.wrap = true
+vim.opt.showtabline = 1
+vim.opt.list = true
+vim.opt.listchars:append("tab:»·")
+vim.opt.listchars:append("trail:·")
 
 -- keymappings [view all the defaults by pressing <leader>Lk]
 lvim.leader = ";"
@@ -46,17 +40,24 @@ lvim.keys.normal_mode["<C-t>"] = ":tabnew<cr>"
 
 -- Change Telescope navigation to use j and k for navigation and n and p for history in both input and normal mode.
 -- lvim.builtin.telescope.on_config_done = function()
---   local actions = require "telescope.actions"
---   -- for input mode
---   lvim.builtin.telescope.defaults.mappings.i["<C-u>"] = false
---   lvim.builtin.telescope.defaults.mappings.i["<C-j>"] = actions.move_selection_next
---   lvim.builtin.telescope.defaults.mappings.i["<C-k>"] = actions.move_selection_previous
---   lvim.builtin.telescope.defaults.mappings.i["<C-n>"] = actions.cycle_history_next
---   lvim.builtin.telescope.defaults.mappings.i["<C-p>"] = actions.cycle_history_prev
---   -- for normal mode
---   lvim.builtin.telescope.defaults.mappings.n["<C-j>"] = actions.move_selection_next
---   lvim.builtin.telescope.defaults.mappings.n["<C-k>"] = actions.move_selection_previous
--- end
+
+local actions = require("telescope.actions")
+lvim.builtin.telescope.defaults.mappings = {
+  i = {
+      ["<esc>"] = actions.close,
+      ["<C-u>"] = false,
+      ["<C-j>"] = actions.move_selection_next,
+      ["<C-k>"] = actions.move_selection_previous,
+      ["<C-n>"] = actions.cycle_history_next,
+      ["<C-p>"] = actions.cycle_history_prev,
+  },
+  n = {
+      ["<C-j>"] = actions.move_selection_next,
+      ["<C-p>"] = actions.move_selection_previous,
+  }
+}
+
+lvim.builtin.terminal.open_mapping = [[<C-\>]]
 
 -- Use which-key to add extra bindings with the leader-key prefix
 lvim.builtin.which_key.mappings["/"] = { ":nohls<CR>", "Clear Search" }
@@ -78,7 +79,7 @@ lvim.builtin.dashboard.active = true
 lvim.builtin.terminal.active = true
 lvim.builtin.nvimtree.setup.view.side = "left"
 lvim.builtin.nvimtree.show_icons.git = 0
-lvim.builtin.nvimtree.setup.view.width = 100
+-- lvim.builtin.nvimtree.setup.view.width = 45
 
 -- if you don't want all the parsers change this to a table of the ones you want
 lvim.builtin.treesitter.ensure_installed = {
@@ -148,16 +149,22 @@ require'lspconfig'.sumneko_lua.setup {
 -- }
 
 -- Additional Plugins
--- lvim.plugins = {
+lvim.plugins = {
+  {"google/vim-jsonnet"},
 --     {"folke/tokyonight.nvim"},
 --     {
 --       "folke/trouble.nvim",
 --       cmd = "TroubleToggle",
 --     },
--- }
+}
 
 -- Autocommands (https://neovim.io/doc/user/autocmd.html)
--- lvim.autocommands.custom_groups = {
---   { "BufWinEnter", "*.lua", "setlocal ts=8 sw=8" },
--- }
+lvim.autocommands.custom_groups = {
+  { "BufWinEnter", "*.md", "setlocal wrap linebreak spell filetype=markdown" },
+  { "BufWinEnter", "*.markdown", "setlocal wrap linebreak spell filetype=markdown" },
+  { "BufWinEnter", "*.tmpl", "setlocal filetype=gotexttmpl" },
+  { "BufRead", "$HOME/.mutt/temp*", "setlocal filetype=mail" },
+  { "Filetype", "mail", "setlocal textwidth=72 wrap spell" },
+  { "Filetype", "mail", "setlocal fo+=aw" },
+}
 

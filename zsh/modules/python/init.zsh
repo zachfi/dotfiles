90f502b5ec0b71865fbfcfafb7bd51b02be8dbb5
Zach Leslie <zach.leslie@grafana.com>
2022-02-09 13:51:26 +0000

    Include python shims in PATH

--- zsh/modules/python/init.zsh
+++ zsh/modules/python/init.zsh
@@ -1,5 +1,6 @@
 if [ -x "$HOME/.pyenv/bin/pyenv" ]; then
   export PYENV_ROOT="$HOME/.pyenv"
   export PATH="$PYENV_ROOT/bin:$PATH"
+  export PATH="$PYENV_ROOT/shims:$PATH"
   eval "$($HOME/.pyenv/bin/pyenv init -)"
 fi

3c35c0bfc345921916a1ba22c86c1c8c9723984e
Zach Leslie <zach.leslie@grafana.com>
2022-06-27 21:41:09 +0000

    Update some keymaps

--- vim/nvim/lua/user/whichkey.lua
+++ vim/nvim/lua/user/whichkey.lua
@@ -90,7 +90,6 @@ local mappings = {
 	["w"] = { "<cmd>w!<CR>", "Save" },
 	["q"] = { "<cmd>q!<CR>", "Quit" },
 	["c"] = { "<cmd>Bdelete!<CR>", "Close Buffer" },
-	["h"] = { "<cmd>nohlsearch<CR>", "No Highlight" },
 	["F"] = { "<cmd>Telescope live_grep theme=ivy<cr>", "Find Text" },
 	["P"] = { "<cmd>lua require('telescope').extensions.projects.projects()<cr>", "Projects" },
 
@@ -127,6 +126,14 @@ local mappings = {
 			"<cmd>Gitsigns diffthis HEAD<cr>",
 			"Diff",
 		},
+	},
+
+	h = {
+		name = "GitHub",
+		i = { "<cmd>Octo search is:open is:issue assignee:zalegrala archived:false<cr>", "My open issues" },
+		I = { "<cmd>Octo issue browser<cr>", "Open issue in browser" },
+		p = { "<cmd>Octo search is:open is:pr author:zalegrala archived:false<cr>", "My open PRs" },
+		P = { "<cmd>Octo pr browser<cr>", "Open PR in browser" },
 	},
 
 	l = {

3860c3b1642119174dece46c51b331b8a13e2f93
Zach Leslie <zach.leslie@grafana.com>
2022-08-22 14:42:00 +0000

    Handle helm files gracefully

--- vim/nvim/lua/user/lsp/handlers.lua
+++ vim/nvim/lua/user/lsp/handlers.lua
@@ -84,6 +84,10 @@ M.on_attach = function(client, bufnr)
 		client.resolved_capabilities.document_formatting = false
 	end
 
+	if vim.bo[bufnr].buftype ~= "" or vim.bo[bufnr].filetype == "helm" then
+		vim.diagnostic.disable()
+	end
+
 	lsp_keymaps(bufnr)
 	local status_ok, illuminate = pcall(require, "illuminate")
 	if not status_ok then

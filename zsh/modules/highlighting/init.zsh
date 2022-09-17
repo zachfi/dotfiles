0bd76cad11ce320c3ac0d4d334b84d9ae0761c5f
Zach Leslie <xaque208@gmail.com>
2022-09-17 19:24:45 +0000

    Add syntax file for linux
    Signed-off-by: Zach Leslie <xaque208@gmail.com>

--- zsh/modules/highlighting/init.zsh
+++ zsh/modules/highlighting/init.zsh
@@ -5,6 +5,8 @@ if [ "${UNAME}" = "Darwin" ]; then
   #export ZSH_HIGHLIGHT_HIGHLIGHTERS_DIR=/usr/local/share/zsh-syntax-highlighting/highlighters
 elif [ "${UNAME}" = "FreeBSD" ]; then
   SYNFILE=/usr/local/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
+elif [ "${UNAME}" = "Linux" ]; then
+  SYNFILE=/usr/share/zsh/plugins/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
 fi
 
 if [ -s "${SYNFILE}" ]; then

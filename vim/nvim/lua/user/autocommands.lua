3b3064db946925c5f5b14955dd7c58cefcc8341b
Zach Leslie <xaque208@gmail.com>
2022-09-17 20:38:16 +0000

    Adjust diagnostics
    Signed-off-by: Zach Leslie <xaque208@gmail.com>

--- vim/nvim/lua/user/autocommands.lua
+++ vim/nvim/lua/user/autocommands.lua
@@ -51,3 +51,18 @@ vim.cmd([[
     autocmd BufWinEnter Tiltfile setlocal filetype=tiltfile syntax=python
   augroup end
 ]])
+
+vim.api.nvim_create_autocmd("CursorHold", {
+	buffer = bufnr,
+	callback = function()
+		local opts = {
+			focusable = false,
+			close_events = { "BufLeave", "CursorMoved", "InsertEnter", "FocusLost" },
+			border = "rounded",
+			source = "always",
+			prefix = " ",
+			scope = "cursor",
+		}
+		vim.diagnostic.open_float(nil, opts)
+	end,
+})

c0393f31f8b190ea704b2a423a9dce58df3b98f8
Zach Leslie <zach.leslie@grafana.com>
2022-10-05 20:07:12 +0000

    Handle deprecations
    Signed-off-by: Zach Leslie <zach.leslie@grafana.com>

--- vim/nvim/lua/user/lsp/handlers.lua
+++ vim/nvim/lua/user/lsp/handlers.lua
@@ -73,23 +73,23 @@ end
 
 M.on_attach = function(client, bufnr)
 	if client.name == "tsserver" then
-		client.resolved_capabilities.document_formatting = false
+		client.server_capabilities.documentFormattingProvider = false
 	end
 
 	if client.name == "sumneko_lua" then
-		client.resolved_capabilities.document_formatting = false
+		client.server_capabilities.documentFormattingProvider = false
 	end
 
 	if client.name == "gopls" then
-		client.resolved_capabilities.document_formatting = false
+		client.server_capabilities.documentFormattingProvider = false
 	end
 
 	if client.name == "terraformls" then
-		client.resolved_capabilities.document_formatting = false
+		client.server_capabilities.documentFormattingProvider = false
 	end
 
 	if client.name == "jsonls" then
-		client.resolved_capabilities.document_formatting = false
+		client.server_capabilities.documentFormattingProvider = false
 	end
 
 	if vim.bo[bufnr].buftype ~= "" or vim.bo[bufnr].filetype == "helm" then

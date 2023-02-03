97a58cfdaeba92c177fb2025778639ec3233627f
Zach Leslie <zach.leslie@grafana.com>
2023-02-03 18:22:00 +0000

    Bring back Q keymap
    Signed-off-by: Zach Leslie <zach.leslie@grafana.com>

--- vim/nvim/lua/user/keymaps.lua
+++ vim/nvim/lua/user/keymaps.lua
@@ -45,6 +45,9 @@ keymap("n", "<S-h>", ":bprevious<CR>", opts)
 keymap("n", "<A-j>", "<Esc>:m .+1<CR>==gi", opts)
 keymap("n", "<A-k>", "<Esc>:m .-2<CR>==gi", opts)
 
+-- Reflow the current line
+keymap("n", "Q", "gw<CR>", opts)
+
 -- Insert --
 -- Press jk fast to enter
 -- keymap("i", "jk", "<ESC>", opts)

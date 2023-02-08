f849e8622f63178a20b2eea459209fb1ad434334
Zach Leslie <xaque208@gmail.com>
2023-02-08 21:50:44 +0000

    Handle deprecations

--- vim/nvim/lua/user/nvim-tree.lua
+++ vim/nvim/lua/user/nvim-tree.lua
@@ -23,12 +23,6 @@ vim.api.nvim_create_autocmd("BufEnter", {
 nvim_tree.setup({
 	disable_netrw = false,
 	hijack_netrw = false,
-	open_on_setup = false,
-	ignore_ft_on_setup = {
-		"startify",
-		"dashboard",
-		"alpha",
-	},
 	open_on_tab = false,
 	hijack_cursor = false,
 	update_cwd = false,

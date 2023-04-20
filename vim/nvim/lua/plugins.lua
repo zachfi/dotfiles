32287daf31905daac72be47d0d5638765915bc73
Zach Leslie <zach.leslie@grafana.com>
2023-04-20 21:03:16 +0000

    More plugin config
    Signed-off-by: Zach Leslie <zach.leslie@grafana.com>

--- vim/nvim/lua/plugins.lua
+++ vim/nvim/lua/plugins.lua
@@ -50,13 +50,18 @@ require("lazy").setup({
 		dependencies = {
 			"nvim-lua/plenary.nvim",
 		},
-		config = function()
-			require("todo-comments").setup({
-				-- your configuration comes here
-				-- or leave it empty to use the default settings
-				-- refer to the configuration section below
-			})
-		end,
+		cmd = { "TodoTrouble", "TodoTelescope" },
+		event = { "BufReadPost", "BufNewFile" },
+		config = true,
+  -- stylua: ignore
+  keys = {
+    { "]t", function() require("todo-comments").jump_next() end, desc = "Next todo comment" },
+    { "[t", function() require("todo-comments").jump_prev() end, desc = "Previous todo comment" },
+    { "<leader>xt", "<cmd>TodoTrouble<cr>", desc = "Todo (Trouble)" },
+    { "<leader>xT", "<cmd>TodoTrouble keywords=TODO,FIX,FIXME<cr>", desc = "Todo/Fix/Fixme (Trouble)" },
+    { "<leader>st", "<cmd>TodoTelescope<cr>", desc = "Todo" },
+    { "<leader>sT", "<cmd>TodoTelescope keywords=TODO,FIX,FIXME<cr>", desc = "Todo/Fix/Fixme" },
+  },
 	},
 
 	{
@@ -75,8 +80,9 @@ require("lazy").setup({
 		priority = 1000, -- make sure to load this before all the other start plugins
 		config = function()
 			-- load the colorscheme here
-			vim.cmd([[colorscheme tokyonight-moon]])
+			vim.cmd([[colorscheme tokyonight]])
 		end,
+		opts = { style = "moon" },
 	},
 	"LunarVim/onedarker",
 	"cocopon/iceberg.vim",
@@ -101,6 +107,11 @@ require("lazy").setup({
 
 	"tomasr/molokai",
 	"ray-x/starry.nvim",
+	{
+		"catppuccin/nvim",
+		lazy = true,
+		name = "catppuccin",
+	},
 
 	-- cmp plugins
 	{

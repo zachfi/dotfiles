5939eb81fa6d7cb1e537affe26e1f4438f6c5d87
Zach Leslie <xaque208@gmail.com>
2023-04-25 03:05:12 +0000

    Relocate plugins file

--- vim/nvim/lua/plugins/init.lua
+++ vim/nvim/lua/plugins/init.lua
@@ -55,6 +55,8 @@ return {
 
 	-- colorschemes
 	"ellisonleao/gruvbox.nvim",
+	"rebelot/kanagawa.nvim
+",
 	{
 		"folke/tokyonight.nvim",
 		lazy = false, -- make sure we load this during startup if it is your main colorscheme
@@ -66,6 +68,7 @@ return {
 		opts = { style = "moon" },
 	},
 	"LunarVim/onedarker",
+  "olimorris/onedarkpro.nvim"
 	"cocopon/iceberg.vim",
 	"jacoborus/tender.vim",
 	"sainnhe/sonokai",

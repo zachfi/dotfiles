06cdb4e338027273664cae2ad91cbfd1b1616e9c
Zach Leslie <zach.leslie@grafana.com>
2023-06-23 15:49:12 +0000

    Tidy up

--- vim/nvim/lua/plugins/init.lua
+++ vim/nvim/lua/plugins/init.lua
@@ -1,118 +1,126 @@
 return {
-	"nvim-lua/popup.nvim", -- An implementation of the Popup API from vim in Neovim
-	"nvim-lua/plenary.nvim", -- Useful lua functions used ny lots of plugins
-	{
-		"nvim-neo-tree/neo-tree.nvim",
-		branch = "v2.x",
-		dependencies = {
-			"nvim-lua/plenary.nvim",
-			"nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
-			"MunifTanjim/nui.nvim",
-		},
-	},
-	--[[ "ahmedkhalf/project.nvim", ]]
-	"antoinemadec/FixCursorHold.nvim", -- This is needed to fix lsp doc highlight
-	--[[ "preservim/tagbar", ]]
+  "nvim-lua/popup.nvim",  -- An implementation of the Popup API from vim in Neovim
+  "nvim-lua/plenary.nvim", -- Useful lua functions used ny lots of plugins
+  {
+    "nvim-neo-tree/neo-tree.nvim",
+    branch = "v2.x",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
+      "MunifTanjim/nui.nvim",
+    },
+    opts = {
+      filesystem = {
+        follow_current_file = true,
+      },
+      buffers = {
+        follow_current_file = true,
+      },
+    },
+  },
+  --[[ "ahmedkhalf/project.nvim", ]]
+  "antoinemadec/FixCursorHold.nvim", -- This is needed to fix lsp doc highlight
+  --[[ "preservim/tagbar", ]]
 
-	{
-		"folke/trouble.nvim",
-		dependencies = {
-			"nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
-		},
-		opts = { use_diagnostic_signs = true },
-	},
+  {
+    "folke/trouble.nvim",
+    dependencies = {
+      "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
+    },
+    opts = { use_diagnostic_signs = true },
+  },
 
-	-- snippets
-	"L3MON4D3/LuaSnip", --snippet engine
-	"rafamadriz/friendly-snippets", -- a bunch of snippets to use
+  -- snippets
+  "L3MON4D3/LuaSnip",            --snippet engine
+  "rafamadriz/friendly-snippets", -- a bunch of snippets to use
 
-	-- Debug
-	"folke/lua-dev.nvim",
-	"mfussenegger/nvim-dap",
-	"theHamsta/nvim-dap-virtual-text",
-	"rcarriga/nvim-dap-ui",
+  -- Debug
+  "folke/lua-dev.nvim",
+  "mfussenegger/nvim-dap",
+  "theHamsta/nvim-dap-virtual-text",
+  "rcarriga/nvim-dap-ui",
 
-	-- Telescope
-	"nvim-telescope/telescope.nvim",
+  -- Telescope
+  "nvim-telescope/telescope.nvim",
 
-	-- Git
-	"tpope/vim-fugitive",
-	"tpope/vim-rhubarb",
+  -- Git
+  "tpope/vim-fugitive",
+  "tpope/vim-rhubarb",
 
-	-- Github
-	"github/copilot.vim",
-	{
-		"pwntester/octo.nvim",
-		dependencies = {
-			"nvim-lua/plenary.nvim",
-			"nvim-telescope/telescope.nvim",
-			"nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
-		},
-		opts = {
-			reaction_viewer_hint_icon = "", -- marker for user reactions
-			user_icon = "👤", -- user icon
-			timeline_marker = "🗨", -- timeline marker
-			right_bubble_delimiter = "", -- Bubble delimiter
-			left_bubble_delimiter = "", -- Bubble delimiter
-		},
-	},
+  -- Github
+  "github/copilot.vim",
+  {
+    "pwntester/octo.nvim",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "nvim-telescope/telescope.nvim",
+      "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
+    },
+    opts = {
+      reaction_viewer_hint_icon = "", -- marker for user reactions
+      user_icon = "👤",           -- user icon
+      timeline_marker = "🗨",     -- timeline marker
+      right_bubble_delimiter = "", -- Bubble delimiter
+      left_bubble_delimiter = "", -- Bubble delimiter
+    },
+  },
 
-	-- ZK for notes
-	{
-		"mickael-menu/zk-nvim",
-		keys = {
-			{ "<leader>zd", ':lua require("zk.commands").get("ZkNew")({ dir = "journal/daily" })<CR>' },
-			{ "<leader>zw", ':lua require("zk.commands").get("ZkNew")({ dir = "journal/weekly" })<CR>' },
-			{ "<leader>zt", ":ZkTags<CR>" },
-			{ "<leader>zo", ":ZkOrphans<CR>" },
-			{ "<leader>zr", ":ZkRecents<CR>" },
-			{ "<leader>za", ":ZkNotes<CR>" },
-			{ "<leader>zn", ":ZkNew<CR>" },
-			{ "<leader>zi", ":ZkIndex<CR>" },
-			{ "<leader>zl", ":ZkLinks<CR>" },
-			{ "<leader>zc", ":ZkCd<CR>" },
-			{ "<leader>zq", ":lua ToggleTodo()<CR>" },
-			{ "<leader>zs", ":!make -C ~/notes update<CR>" },
-		},
+  -- ZK for notes
+  {
+    "mickael-menu/zk-nvim",
+    keys = {
+      { "<leader>zd", ':lua require("zk.commands").get("ZkNew")({ dir = "journal/daily" })<CR>' },
+      { "<leader>zw", ':lua require("zk.commands").get("ZkNew")({ dir = "journal/weekly" })<CR>' },
+      { "<leader>zt", ":ZkTags<CR>" },
+      { "<leader>zo", ":ZkOrphans<CR>" },
+      { "<leader>zr", ":ZkRecents<CR>" },
+      { "<leader>za", ":ZkNotes<CR>" },
+      { "<leader>zn", ":ZkNew<CR>" },
+      { "<leader>zi", ":ZkIndex<CR>" },
+      { "<leader>zl", ":ZkLinks<CR>" },
+      { "<leader>zc", ":ZkCd<CR>" },
+      { "<leader>zq", ":lua ToggleTodo()<CR>" },
+      { "<leader>zs", ":!make -C ~/notes update<CR>" },
+    },
 
-		config = function()
-			local zk = require("zk")
-			zk.setup({
-				{
-					picker = "telescope",
+    config = function()
+      local zk = require("zk")
+      zk.setup({
+        {
+          picker = "telescope",
 
-					lsp = {
-						config = {
-							root_dir = "/home/zach/notes",
-						},
-					},
-				},
-			})
+          lsp = {
+            config = {
+              root_dir = "/home/zach/notes",
+            },
+          },
+        },
+      })
 
-			local commands = require("zk.commands")
+      local commands = require("zk.commands")
 
-			local function make_edit_fn(defaults, picker_options)
-				return function(options)
-					options = vim.tbl_extend("force", defaults, options or {})
-					zk.edit(options, picker_options)
-				end
-			end
+      local function make_edit_fn(defaults, picker_options)
+        return function(options)
+          options = vim.tbl_extend("force", defaults, options or {})
+          zk.edit(options, picker_options)
+        end
+      end
 
-			commands.add("ZkOrphans", function(options)
-				options = vim.tbl_extend("force", { orphan = true }, options or {})
-				zk.edit(options, { title = "Zk Orphans" })
-			end)
+      commands.add("ZkOrphans", function(options)
+        options = vim.tbl_extend("force", { orphan = true }, options or {})
+        zk.edit(options, { title = "Zk Orphans" })
+      end)
 
-			commands.add("ZkRecents", make_edit_fn({ createdAfter = "2 weeks ago" }, { title = "Zk Recents" }))
-		end,
-	},
-	{
-		"dstein64/vim-startuptime",
-		-- lazy-load on a command
-		cmd = "StartupTime",
-		-- init is called during startup. Configuration for vim plugins typically should be set in an init function
-		init = function()
-			vim.g.startuptime_tries = 10
-		end,
-	},
+      commands.add("ZkRecents", make_edit_fn({ createdAfter = "2 weeks ago" }, { title = "Zk Recents" }))
+    end,
+  },
+  {
+    "dstein64/vim-startuptime",
+    -- lazy-load on a command
+    cmd = "StartupTime",
+    -- init is called during startup. Configuration for vim plugins typically should be set in an init function
+    init = function()
+      vim.g.startuptime_tries = 10
+    end,
+  },
 }
--- vim/nvim/lua/plugins/lsp/format.lua
+++ vim/nvim/lua/plugins/lsp/format.lua
@@ -3,35 +3,40 @@ local M = {}
 M.autoformat = true
 
 function M.on_attach(client, buf)
-	-- dont format if client disabled it
+  -- dont format if client disabled it
 
-	-- default lsp format function
-	local f = vim.lsp.buf.format
+  -- default lsp format function
+  local f = vim.lsp.buf.format
 
-	if client.name == "gopls" then
-		--[[   client.server_capabilities.documentFormattingProvider = false ]]
-		f = require("go.format").gofmt
-	end
+  if client.name == "gopls" then
+    --[[   client.server_capabilities.documentFormattingProvider = false ]]
+    local go = require("go.format")
 
-	if
-		client.config
-		and client.config.capabilities
-		and client.config.capabilities.documentFormattingProvider == false
-	then
-		return
-	end
+    f = function()
+      require("go.format").gofmt()
+      require("go.format").goimport()
+    end
+  end
 
-	if client.supports_method("textDocument/formatting") then
-		vim.api.nvim_create_autocmd("BufWritePre", {
-			group = vim.api.nvim_create_augroup("LspFormat." .. buf, {}),
-			buffer = buf,
-			callback = function()
-				if M.autoformat then
-					f()
-				end
-			end,
-		})
-	end
+  if
+      client.config
+      and client.config.capabilities
+      and client.config.capabilities.documentFormattingProvider == false
+  then
+    return
+  end
+
+  if client.supports_method("textDocument/formatting") then
+    vim.api.nvim_create_autocmd("BufWritePre", {
+      group = vim.api.nvim_create_augroup("LspFormat." .. buf, {}),
+      buffer = buf,
+      callback = function()
+        if M.autoformat then
+          f()
+        end
+      end,
+    })
+  end
 end
 
 return M

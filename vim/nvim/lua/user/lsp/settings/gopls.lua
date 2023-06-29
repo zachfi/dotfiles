b7904b3471b678476ff68ca51b81d1d45688cc1c
Zach Leslie <zach.leslie@grafana.com>
2023-06-29 22:03:11 +0000

    Updates for go

--- vim/nvim/lua/user/lsp/settings/gopls.lua
+++ vim/nvim/lua/user/lsp/settings/gopls.lua
@@ -4,14 +4,16 @@ local opts = {
 			-- buildFlags = { "-tags=requires_docker" }, -- custom build flags
 			experimentalPostfixCompletions = true,
 			analyses = {
-				unusedparams = true,
-				shadow = true,
+				fieldalignment = true,
 				nilness = true,
+				shadow = true,
+				unusedparams = true,
 				unusedwrite = true,
 				useany = true,
 			},
 			staticcheck = true,
 			codelenses = {
+				gc_details = true,
 				upgrade_dependency = true,
 				run_govulncheck = true,
 				tidy = true,

bd955c6f7eb571276c4891a418b066317a39ff3e
Zach Leslie <zach.leslie@grafana.com>
2023-10-02 17:02:36 +0000

    Updates for blankline module

--- vim/nvim/lua/plugins/ui.lua
+++ vim/nvim/lua/plugins/ui.lua
@@ -127,11 +127,16 @@ return {
     event = { "BufReadPost", "BufNewFile" },
     main = "ibl",
     opts = {
+      indent = { char = "│" },
+      scope = {
+        show_start = false,
+        show_end = false,
+      },
       -- char = "▏",
-      char = "│",
-      filetype_exclude = { "help", "alpha", "dashboard", "neo-tree", "Trouble", "lazy" },
-      show_trailing_blankline_indent = false,
-      show_current_context = false,
+      --[[ char = "│", ]]
+      --[[ filetype_exclude = { "help", "alpha", "dashboard", "neo-tree", "Trouble", "lazy" }, ]]
+      --[[ show_trailing_blankline_indent = false, ]]
+      --[[ show_current_context = false, ]]
     },
   },
 

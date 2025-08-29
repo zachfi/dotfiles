647eca3bf5aa2c4098f68c78ac8312022d30af3b
Zach Leslie <zach.leslie@grafana.com>
2025-08-29 21:30:28 +0000

    Updates

--- vim/nvim/lua/plugins/coding.lua
+++ vim/nvim/lua/plugins/coding.lua
@@ -141,7 +141,53 @@ return {
   {
     "hedyhli/outline.nvim",
     config = function()
-      require("outline").setup({})
+      local icons = {
+        Function = "󰊕",
+        Method = "󰡱",
+      }
+
+      require("outline").setup({
+        symbols = {
+          icon_fetcher = function(k)
+            return icons[k] or false
+          end,
+
+          -- icons = {
+          --   File = { icon = "󰈔", hl = "Identifier" },
+          --   Module = { icon = "󰆧", hl = "Include" },
+          --   Namespace = { icon = "󰅪", hl = "Include" },
+          --   Package = { icon = "󰏗", hl = "Include" },
+          --   Class = { icon = "𝓒", hl = "Type" },
+          --   Method = { icon = "ƒ", hl = "Function" }, -- another one
+          --   Property = { icon = "", hl = "Identifier" },
+          --   Field = { icon = "󰆨", hl = "Identifier" },
+          --   Constructor = { icon = "", hl = "Special" },
+          --   Enum = { icon = "ℰ", hl = "Type" },
+          --   Interface = { icon = "󰜰", hl = "Type" },
+          --   Function = { icon = "", hl = "Function" }, -- the only override from default
+          --   Variable = { icon = "", hl = "Constant" },
+          --   Constant = { icon = "", hl = "Constant" },
+          --   String = { icon = "𝓐", hl = "String" },
+          --   Number = { icon = "#", hl = "Number" },
+          --   Boolean = { icon = "⊨", hl = "Boolean" },
+          --   Array = { icon = "󰅪", hl = "Constant" },
+          --   Object = { icon = "⦿", hl = "Type" },
+          --   Key = { icon = "🔐", hl = "Type" },
+          --   Null = { icon = "NULL", hl = "Type" },
+          --   EnumMember = { icon = "", hl = "Identifier" },
+          --   Struct = { icon = "𝓢", hl = "Structure" },
+          --   Event = { icon = "🗲", hl = "Type" },
+          --   Operator = { icon = "+", hl = "Identifier" },
+          --   TypeParameter = { icon = "𝙏", hl = "Identifier" },
+          --   Component = { icon = "󰅴", hl = "Function" },
+          --   Fragment = { icon = "󰅴", hl = "Constant" },
+          --   TypeAlias = { icon = " ", hl = "Type" },
+          --   Parameter = { icon = " ", hl = "Identifier" },
+          --   StaticMethod = { icon = " ", hl = "Function" },
+          --   Macro = { icon = " ", hl = "Function" },
+          -- },
+        },
+      })
     end,
   },
 
